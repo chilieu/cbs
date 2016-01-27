99aa0185ca34db623eb4cb3f2b061b4ded3472be
Chi LT <lieu.thanh.chi@gmail.com>
2016-01-27 04:56:42 +0000

    added download

--- application/controllers/products/Index.php
+++ application/controllers/products/Index.php
@@ -22,9 +22,9 @@ class Index extends Front_Controller
 		//if not PayPalAdvancedNop link
 		if( $name != "PayPal-advanced-nop" ) return;
 		$data = time() . '-' . $this->input->ip_address() . "-" . $this->input->user_agent();
-		file_put_contents('/files/PayPal-advanced-nop.log', $data, FILE_APPEND | LOCK_EX);
+		file_put_contents( realpath(dirname(__DIR__)) . '../public/files/PayPal-advanced-nop.log', $data, FILE_APPEND | LOCK_EX);
 
-		$data = file_get_contents("/files/PayPalAdvancedNop3.70.zip"); // Read the file's contents
+		$data = file_get_contents( realpath(dirname(__DIR__)) .  "../public/files/PayPalAdvancedNop3.70.zip"); // Read the file's contents
 		force_download($name . ".zip", $data);
 	}
 
